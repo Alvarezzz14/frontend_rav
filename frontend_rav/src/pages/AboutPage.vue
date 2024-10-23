8907e1b044ac09d166346ee0621e15a744e3d64e
Alvarezzz14 <andresalvarez14@gmail.com>
2024-10-23 14:16:16 -0500

    routerConfig

---
+++ frontend_rav/src/pages/AboutPage.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h1>About Us</h1>
+		<p>This is the About Page.</p>
+	</div>
+</template>
+
+<script setup>
+// Si necesitas agregar lógica para el AboutPage, puedes hacerlo aquí.
+// Si no hay lógica de script, el componente se mostrará igual.
+</script>
