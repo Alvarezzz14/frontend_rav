453d58ab2a5a4e412630c941cb76fa1b3e5adb56
KateToro29 <katerine29echeverry@gmail.com>
2024-10-24 13:18:39 -0500

    se agregan estilos al titulo

--- frontend_rav/src/components/Titulo.vue
+++ frontend_rav/src/components/Titulo.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="text-2xl w-full h-20 p-4 flex flex-col">
-        <div>
-            <h2>Departamentos de</h2> <!-- Título principal alineado a la izquierda -->
-        </div> <!-- Alineado a la izquierda -->
-      <div class="flex items-center mt-1"> <!-- Alineación del texto "de Colombia" -->
-        <h1 class="font-bold text-customPurple ">Colombia</h1>
+      <div class="w-[80%] md:w-[392px] h-[10vh] md:h-[42px] absolute top-[10%] md:top-[84px] left-[10%] md:left-[333px] gap-2 md:gap-[10px] font-sans text-[24px] md:text-[36px] font-normal leading-[42.23px] text-center">
+            <h2 class="text-black" >Departamentos de</h2> <!-- Título principal alineado a la izquierda -->
+      </div> <!-- Alineado a la izquierda -->
+      <div class="w-96 h-24  "> <!-- Alineación del texto "de Colombia" -->
+        <h1 class="font-bold text-5xl text-customPurple ">Colombia</h1>
       </div>
     </div>
   </template>
