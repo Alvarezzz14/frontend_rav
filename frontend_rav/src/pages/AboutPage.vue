8dd1505517f620c71c4505c790d81ab9537eb5f2
Andres Alvarez <120440511+Alvarezzz14@users.noreply.github.com>
2024-10-23 20:56:38 -0500

    Revert "Se agrega mapa al codigo, pero aun no se logra correr perfectamente"

---
+++ frontend_rav/src/pages/AboutPage.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<h1>About Us</h1>
+		<p class="text-red-200">This is the About Page.</p>
+	</div>
+</template>
+
+<script setup>
+// Si necesitas agregar lógica para el AboutPage, puedes hacerlo aquí.
+// Si no hay lógica de script, el componente se mostrará igual.
+</script>
