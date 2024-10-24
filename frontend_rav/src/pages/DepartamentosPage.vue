3167797ac2000a50ca9d4843f082c57f4f582a3e
KateToro29 <katerine29echeverry@gmail.com>
2024-10-24 14:40:41 -0500

    se agrega el mapa de colombia

--- frontend_rav/src/pages/DepartamentosPage.vue
+++ frontend_rav/src/pages/DepartamentosPage.vue
@@ -1,11 +1,22 @@
 <template>
-    <Titulo></Titulo>
-    <SearchApartament></SearchApartament>
-    
-
+    <div class="flex">
+        <div class="w-1/2 p-4">
+            <div class="mb-4">
+                <Titulo></Titulo>
+            </div>
+            <div>
+                <SearchApartament></SearchApartament>
+            </div>
+        </div>
+        <div class="w-1/2 p-4">
+            <MapaColombia></MapaColombia>
+        </div>
+    </div>
 </template>
+
 <script setup>
 
+import MapaColombia from '../components/MapaColombia.vue';
 import SearchApartament from '../components/SearchApartament.vue';
 import Titulo from '../components/Titulo.vue';
 
