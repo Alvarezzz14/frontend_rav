e6f377a2571778669947f6118d8e501e28d92374
Alvarezzz14 <andresalvarez14@gmail.com>
2024-10-24 15:00:32 -0500

    tamaño mapa

--- frontend_rav/src/pages/DepartamentosPage.vue
+++ frontend_rav/src/pages/DepartamentosPage.vue
@@ -1,23 +1,21 @@
 <template>
-    <div class="flex">
-        <div class="w-1/2 p-4">
-            <div class="mb-4">
-                <Titulo></Titulo>
-            </div>
-            <div>
-                <SearchApartament></SearchApartament>
-            </div>
-        </div>
-        <div class="w-1/2 p-4">
-            <MapaColombia></MapaColombia>
-        </div>
-    </div>
+	<div class="flex">
+		<div class="w-1/2 p-4">
+			<div class="mb-4">
+				<Titulo></Titulo>
+			</div>
+			<div>
+				<SearchApartament></SearchApartament>
+			</div>
+		</div>
+		<div class="w-1/2 p-4">
+			<MapaColombia></MapaColombia>
+		</div>
+	</div>
 </template>
 
 <script setup>
-
-import MapaColombia from '../components/MapaColombia.vue';
-import SearchApartament from '../components/SearchApartament.vue';
-import Titulo from '../components/Titulo.vue';
-
+import MapaColombia from "../components/MapaColombia.vue";
+import SearchApartament from "../components/SearchApartament.vue";
+import Titulo from "../components/Titulo.vue";
 </script>
