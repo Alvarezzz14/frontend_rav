146391176f5a3c39d86df6f205b139d3649c9d47
KateToro29 <katerine29echeverry@gmail.com>
2024-10-24 11:11:35 -0500

    se agrega componente de titulo

---
+++ frontend_rav/src/components/Titulo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="text-2xl w-full h-20 p-4 flex flex-col">
+        <div>
+            <h2>Departamentos de</h2> <!-- Título principal alineado a la izquierda -->
+        </div> <!-- Alineado a la izquierda -->
+      <div class="flex items-center mt-1"> <!-- Alineación del texto "de Colombia" -->
+        <h1 class="font-bold text-customPurple ">Colombia</h1>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  /* Puedes añadir estilos adicionales aquí si es necesario */
+  </style>
+  
