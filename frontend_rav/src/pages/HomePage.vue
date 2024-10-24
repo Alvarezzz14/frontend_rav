82bf686d8cfe7ed109c4a8a83c214e4fdfcae621
KateToro29 <katerine29echeverry@gmail.com>
2024-10-23 21:50:22 -0500

    se agrega componente de SearchApartament.vue que nos permite seleccionar el departamento y adicionalmente se le agrega targeta informativa de la selección anterior

--- frontend_rav/src/pages/HomePage.vue
+++ frontend_rav/src/pages/HomePage.vue
@@ -2,10 +2,14 @@
 	<div>
 		<h1 class="text-3xl font-bold text-center">Home Page</h1>
 		<p class="text-lg text-center">Bienvenido a la página de inicio</p>
+		<SearchApartament></SearchApartament>
+		
 	</div>
 	<Button />
 </template>
 
 <script setup>
 import Button from "../components/Button.vue";
+
+import SearchApartament from "../components/SearchApartament.vue";
 </script>
