d5c5034b6e0f52f15cc515bcb824e5f1c43aa4b8
Alvarezzz14 <andresalvarez14@gmail.com>
2024-10-24 14:24:34 -0500

    detallosos

--- frontend_rav/src/App.vue
+++ frontend_rav/src/App.vue
@@ -1,10 +1,7 @@
 <template>
 	<div>
-	
 		<router-view />
 	</div>
 </template>
 
-<script setup>
-// No se necesita lógica aquí por ahora, ya que estamos manteniendo el archivo lo más limpio posible.
-</script>
+<script setup></script>
