16b851afa04bf14736b8fc7652b39962404d239c
Alvarezzz14 <andresalvarez14@gmail.com>
2024-10-24 20:31:09 -0500

    arreglo router

--- frontend_rav/src/router/index.js
+++ frontend_rav/src/router/index.js
@@ -3,7 +3,7 @@ import DefaultLayout from '../layouts/DefaultLayout.vue';
 import HomePage from '../pages/HomePage.vue'; // Importa las páginas
 import DepartamentosPage from '../pages/DepartamentosPage.vue';
 import TimeLine from '../pages/TimeLine.vue';
-import LoginPage from '../pages/LoginPage.vue'
+import LoginPage from '../pages/LoginPage.vue';
 
 const routes = [
   {
@@ -22,31 +22,14 @@ const routes = [
       {
         path: '/DepartamentosPage',
         component: DepartamentosPage // pagina que se renderiza dentro del layout
+      },
+      {
+        path: '/TimeLine',
+        component: TimeLine // pagina que se renderiza dentro del layout
       }
     ]
   },
 
-
-const routes = [
-  
-    {
-        path: '/',
-        component: DefaultLayout, // Usa el layout como contenedor
-        children: [
-            {
-                path: '',
-                component: HomePage // pagina que se renderiza dentro del layout
-            },
-            {
-                path: '/DepartamentosPage',
-                component: DepartamentosPage // pagina que se renderiza dentro del layout
-            },
-            {
-                path: '/TimeLine',
-                component: TimeLine // pagina que se renderiza dentro del layout
-            }
-        ]
-    },
 ];
 
 const router = createRouter({
