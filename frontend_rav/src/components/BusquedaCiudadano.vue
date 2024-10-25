6991d0717ea60bb6fbc2ee0deea57e08c85d480b
KateToro29 <katerine29echeverry@gmail.com>
2024-10-25 16:37:09 -0500

    se agregan imagenes a la carpeta assets y se cambia la configuracion para que las rutas sean relativas

--- frontend_rav/src/components/BusquedaCiudadano.vue
+++ frontend_rav/src/components/BusquedaCiudadano.vue
@@ -1,14 +1,26 @@
 <template>
-    <div class="flex flex-col lg:flex-row items-center justify-center p-6 bg-gray-100 h-full">
-      <!-- Sección de texto -->
-      <div class="mb-6 lg:mb-0 lg:w-1/2">
-        <p class="text-xl lg:text-3xl font-semibold text-inherit">
-          Aquí podrás visualizar información acerca de la Ruta de Atención al Ciudadano.
+  <div class="flex flex-col lg:flex-row items-center justify-center p-6 bg-gray-100 h-full">
+    <!-- Imagen del ciudadano a la izquierda -->
+    <div class="lg:w-1/3 flex justify-center lg:justify-start mb-6 lg:mb-0">
+      <img
+        :src="Ciudadano"
+        alt="Ciudadano"
+        class="w-20 h-20 lg:w-32 lg:h-32 object-contain"
+      />
+    </div>
+
+    <!-- Texto y campo de búsqueda a la derecha -->
+    <div class="flex flex-col items-center lg:items-start lg:w-2/3 lg:pl-10">
+      <!-- Texto de información -->
+      <div class="mb-6">
+        <p class="text-xl lg:text-3xl font-medium text-inherit">
+          Aquí podrás visualizar información acerca de la 
         </p>
+        <h2 class="text-9xl lg:text-3xl font-bold  text-customPurple "> Ruta de  Atención al Ciudadano.</h2>
       </div>
-  
+
       <!-- Sección de búsqueda -->
-      <div class="w-full lg:w-1/3 bg-white p-5 rounded-2xl shadow-lg">
+      <div class="w-full lg:w-2/3 bg-white p-5 rounded-2xl shadow-lg">
         <div class="flex flex-col items-center">
           <input
             type="text"
@@ -21,9 +33,18 @@
         </div>
       </div>
     </div>
-  </template>
+  </div>
+</template>
+
+
+
+<style scoped>
+/* Estilos adicionales si son necesarios */
+</style>
+
   
   <script>
+  import Ciudadano from '../assets/images/Ciudadano.svg'
   export default {
     name: "RutaCiudadano",
   };
