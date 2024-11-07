f8ec2d6ac70564745129e3ed9cf6b6cc27116558
Alvarezzz14 <andresalvarez14@gmail.com>
2024-11-06 22:20:50 -0500

    CambiosPara busqueda CIudadno

--- frontend_rav/src/components/BusquedaCiudadano.vue
+++ frontend_rav/src/components/BusquedaCiudadano.vue
@@ -1,49 +1,52 @@
 <template>
-	<div class="flex flex-col lg:flex-row items-center justify-center p-6 bg-gray-100 h-full">
-	  <div class="lg:w-1/3 flex justify-center lg:justify-start mb-6 lg:mb-0">
-		<img :src="Ciudadano" alt="Ciudadano" class="w-96 h-fit object-contain" />
-	  </div>
-	  <div class="flex flex-col items-center lg:items-start lg:w-2/3 lg:pl-10">
-		<div class="mb-6">
-		  <p class="text-xl lg:text-3xl font-medium text-inherit">
-			Aquí podrás visualizar información acerca de la
-		  </p>
-		  <h2 class="lg:text-5xl font-bold text-customPurple">Ruta de Atención al Ciudadano.</h2>
+	<div
+		class="flex flex-col lg:flex-row items-center justify-center p-6 bg-gray-100 h-full">
+		<div class="lg:w-1/3 flex justify-center lg:justify-start mb-6 lg:mb-0">
+			<img :src="Ciudadano" alt="Ciudadano" class="w-96 h-fit object-contain" />
 		</div>
-		<div class="w-full lg:w-2/3 bg-white p-5 rounded-2xl shadow-lg">
-		  <div class="flex flex-col items-center">
-			<input
-			  v-model="searchCedula"
-			  @keydown.enter="searchUser"
-			  type="text"
-			  placeholder="Ingrese número de documento"
-			  class="w-full p-4 border text-customPurple border-customPurple rounded-lg"
-			/>
-			<Button
-			  type="button"
-			  label="Buscar"
-			  icon="pi pi-search"
-			  :loading="loading"
-			  class="mt-4 w-full lg:w-auto !hover:bg-purple-600 !border-none !bg-customPurple text-white"
-			  @click="searchUser"
-			/>
-		  </div>
+		<div class="flex flex-col items-center lg:items-start lg:w-2/3 lg:pl-10">
+			<div class="mb-6">
+				<p class="text-xl lg:text-3xl font-medium text-inherit">
+					Aquí podrás visualizar información acerca de la
+				</p>
+				<h2 class="lg:text-5xl font-bold text-customPurple">
+					Ruta de Atención al Ciudadano.
+				</h2>
+			</div>
+			<div class="w-full lg:w-2/3 bg-white p-5 rounded-2xl shadow-lg">
+				<div class="flex flex-col items-center">
+					<input
+						v-model="searchCedula"
+						@keydown.enter="searchUser"
+						type="text"
+						placeholder="Ingrese número de documento"
+						class="w-full p-4 border text-customPurple border-customPurple rounded-lg" />
+					<Button
+						type="button"
+						label="Buscar"
+						icon="pi pi-search"
+						:loading="loading"
+						class="mt-4 w-full lg:w-auto !hover:bg-purple-600 !border-none !bg-customPurple text-white"
+						@click="searchUser" />
+				</div>
+			</div>
 		</div>
-	  </div>
-	  <Dialog
-		v-model:visible="noResultsModal"
-		modal
-		header="Búsqueda Fallida"
-		:style="{ width: '30rem' }"
-	  >
-		<p class="m-0 text-customPurple">
-		  {{ modalMessage }}
-		  <strong>{{ searchCedula }}</strong>
-		</p>
-		<template #footer>
-		  <Button label="Cerrar" @click="noResultsModal = false" class="p-button-text" />
-		</template>
-	  </Dialog>
+		<Dialog
+			v-model:visible="noResultsModal"
+			modal
+			header="Búsqueda Fallida"
+			:style="{ width: '30rem' }">
+			<p class="m-0 text-customPurple">
+				{{ modalMessage }}
+				<strong>{{ searchCedula }}</strong>
+			</p>
+			<template #footer>
+				<Button
+					label="Cerrar"
+					@click="noResultsModal = false"
+					class="p-button-text" />
+			</template>
+		</Dialog>
 	</div>
 </template>
 
@@ -54,7 +57,7 @@ import { useRouter } from "vue-router";
 import { useEventStore } from "@/stores/storedataOff.js";
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
-import axios from 'axios';
+import axios from "axios";
 
 const searchCedula = ref("");
 const noResultsModal = ref(false);
@@ -63,13 +66,15 @@ const loading = ref(false);
 const router = useRouter();
 const eventStore = useEventStore();
 async function searchByCedula(cedula) {
-  try {
-    const response = await axios.get(`http://localhost:8081/api/v1/victimas/${cedula}`);
-    return response.data; // Aquí accedemos directamente a los datos JSON
-  } catch (error) {
-    console.error('Error al buscar eventos por cédula:', error);
-    return [];
-  }
+	try {
+		const response = await axios.get(
+			`http://localhost:8082/api/v1/victimas/${cedula}`
+		);
+		return response.data; // Aquí accedemos directamente a los datos JSON
+	} catch (error) {
+		console.error("Error al buscar eventos por cédula:", error);
+		return [];
+	}
 }
 
 // Modifica para usar la API
@@ -82,10 +87,10 @@ const searchUser = async () => {
 
 	loading.value = true;
 	const results = await searchByCedula(searchCedula.value);
-	
-	console.log(results)
+
+	console.log(results);
 	if (Object.keys(results).length > 0) {
-		eventStore.setUserInfo(results)
+		eventStore.setUserInfo(results);
 		router.push({
 			name: "PerfilUsuarioPage",
 			params: { userInfo: results },
@@ -96,8 +101,5 @@ const searchUser = async () => {
 	}
 
 	loading.value = false;
-
 };
-
-  </script>
-  
+</script>
--- frontend_rav/src/stores/storedataOff.js
+++ frontend_rav/src/stores/storedataOff.js
@@ -1,13 +1,10 @@
-// storedataOff.js
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import axios from 'axios';
-
 
 export const useEventStore = defineStore('eventStore', () => {
   const userInfo = ref({});
-  const setUserInfo = (newUserInfo) => {userInfo.value = newUserInfo}
-  
+  const setUserInfo = (newUserInfo) => { userInfo.value = newUserInfo }
+
   return { userInfo, setUserInfo };
 
 });
