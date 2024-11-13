69bef97a9f66de461e5769e9be5d6f1380e2272f
KateToro29 <katerine29echeverry@gmail.com>
2024-11-13 15:46:20 -0500

    se agrega responsive a los GOV de Header y Footer

--- frontend_rav/src/components/Footer.vue
+++ frontend_rav/src/components/Footer.vue
@@ -1,8 +1,8 @@
 <template>
 	<footer class=" w-full h-16 items-center justify-between  relative">
 		<!-- Ícono institucional a la izquierda con fondo azul -->
-		<div class="flex items-center bg-azulHeaderFooter">
-			<img :src="IconoGov" alt="" class="w-[140px] h-6 top-[1000px] ml-2 left-[20px] my-2">
+		<div class="flex items-center justify-center w-full sm:w-auto sm:justify-start bg-azulHeaderFooter">
+			<img :src="IconoGov" alt="Icono Institucional" class="w-[140px] h-6 ml-2 sm:ml-0 my-2">
 		</div>
 
 		<!-- Texto centrado con fondo negro -->
--- frontend_rav/src/components/Header.vue
+++ frontend_rav/src/components/Header.vue
@@ -1,18 +1,17 @@
 <template>
-	<header
-		class="w-full h-12 bg-azulHeaderFooter flex items-center justify-between px-4">
-		<!-- Icono SVG institucional a la izquierda -->
-		<div class="flex items-center bg-azulHeaderFooter">
-			<img :src="IconoGov" alt="" class="w-[140px] h-6 top-[1000px] ml-2 left-[20px] my-2">
-		</div>
-
-		<!-- Otras opciones del header a la derecha, si es necesario -->
-		<div>
-			<!-- Aquí puedes poner elementos adicionales si es necesario -->
-		</div>
+	<header class="w-full h-12 bg-azulHeaderFooter flex items-center justify-between px-4">
+	  <!-- Icono SVG institucional -->
+	  <div class="flex items-center justify-center w-full sm:w-auto sm:justify-start bg-azulHeaderFooter">
+		<img :src="IconoGov" alt="Icono Institucional" class="w-[140px] h-6 ml-2 sm:ml-0 my-2">
+	  </div>
+  
+	  <!-- Otras opciones del header a la derecha, si es necesario -->
+	  <div class="hidden sm:block">
+		<!-- Aquí puedes poner elementos adicionales si es necesario -->
+	  </div>
 	</header>
-</template>
-
+  </template>
+  
 <script setup>
 // Si tienes el SVG como un componente, lo importas aquí
 import IconoGov from "@/assets/images/Gov.svg"
