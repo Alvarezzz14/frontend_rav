b9ac1a7dcac10e23a8acf26745da5fbe4b6bddac
darwmart <darwmart1987@gmail.com>
2024-11-19 16:17:13 -0500

    nose

--- frontend_rav/src/components/BusquedaCiudadano.vue
+++ frontend_rav/src/components/BusquedaCiudadano.vue
@@ -1,53 +1,53 @@
 <template> 
-<div class="block lg:flex items-center justify-center p-6 bg-grisFondo h-full">
-	 <div class="flex flex-col items-center lg:items-center lg:w-2/3 lg:pl-10"> 
-		<div class="mb-4 w-2/3">
-			 <p class="text-xl lg:text-3xl font-medium text-inherit"> Aquí podrás visualizar información acerca de la </p> 
-			<h2 class="lg:text-6xl font-bold text- text-azulBarraApe"> Ruta de Atención al Ciudadano. </h2>
-		 </div> 
-		 <div class="w-full lg:w-2/3 bg-white p-5 rounded-2xl shadow-lg">
-			 <div class="flex flex-col items-center">
-                    <input
-                        v-model="searchCedula"
-                        @keydown.enter="searchUser"
-                        type="text"
-                        placeholder="Ingrese número de documento"
-                        class="w-full p-4 border text-black border-none rounded-lg bg-grisFondo" />
-                    <Button
-                        type="button"
-                        label="Buscar"
-                        icon="pi pi-search"
-                        :loading="loading"
-                        class="mt-4 w-full p-4  !hover:bg-purple-600 !border-none !bg-azulBarraApe !text-yellow-500 "
-                        @click="searchUser" />
+    <div class="block lg:flex items-center justify-center p-6 bg-grisFondo h-full">
+         <div class="flex flex-col items-center lg:items-center lg:w-2/3 lg:pl-10"> 
+            <div class="mb-4 w-2/3">
+                 <p class="text-xl lg:text-3xl font-medium text-inherit"> Aquí podrás visualizar información acerca de la </p> 
+                <h2 class="lg:text-6xl font-bold text- text-azulBarraApe"> Ruta de Atención al Ciudadano. </h2>
+             </div> 
+             <div class="w-full lg:w-2/3 bg-white p-5 rounded-2xl shadow-lg">
+                 <div class="flex flex-col items-center">
+                        <input
+                            v-model="searchCedula"
+                            @keydown.enter="searchUser"
+                            type="text"
+                            placeholder="Ingrese número de documento"
+                            class="w-full p-4 border text-black border-none rounded-lg bg-grisFondo" />
+                        <Button
+                            type="button"
+                            label="Buscar"
+                            icon="pi pi-search"
+                            :loading="loading"
+                            class="mt-4 w-full p-4  !hover:bg-purple-600 !border-none !bg-azulBarraApe !text-yellow-500 "
+                            @click="searchUser" />
+                    </div>
                 </div>
             </div>
-        </div>
-    <div class="flex items-center justify-center lg:w-2/3">
-            <img src="@/assets/images/CiudadanoRutaAtencion.svg" alt="Persona sonriendo" class="rounded-lg object-cover w-2/3 max-h-96 xl:max-h-full" />
-</div>
-
-        <Dialog
-            v-model:visible="noResultsModal"
-            modal
-            header="Búsqueda Fallida"
-            :style="{ width: '30rem' }">
-            <p class="m-0 text-customPurple">
-                {{ modalMessage }}
-                <strong>{{ searchCedula }}</strong>
-            </p>
-            <template #footer>
-                <Button
-                    label="Cerrar"
-                    @click="noResultsModal = false"
-                    class="p-button-text" />
-            </template>
-        </Dialog>
+        <div class="flex items-center justify-center lg:w-2/3">
+                <img src="@/assets/images/CiudadanoRutaAtencion.svg" alt="Persona sonriendo" class="rounded-lg object-cover w-2/3 max-h-96 xl:max-h-full" />
     </div>
-</template>
+    
+            <Dialog
+                v-model:visible="noResultsModal"
+                modal
+                header="Búsqueda Fallida"
+                :style="{ width: '30rem' }">
+                <p class="m-0 text-customPurple">
+                    {{ modalMessage }}
+                    <strong>{{ searchCedula }}</strong>
+                </p>
+                <template #footer>
+                    <Button
+                        label="Cerrar"
+                        @click="noResultsModal = false"
+                        class="p-button-text" />
+                </template>
+            </Dialog>
+        </div>
+    </template>
 
  <script setup>
-import CiudadanoRutaAtencion from '@/assets/images/CiudadanoRutaAtencion.svg';
+import Ciudadano from '@/assets/images/Ciudadano.svg';
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useEventStore } from "@/stores/storedataOff.js";
--- frontend_rav/src/components/FormatodeReportes.vue
+++ frontend_rav/src/components/FormatodeReportes.vue
@@ -15,7 +15,7 @@
           <h2 class="text-azulBarraApe text-5xl md:text-6xl mt-0 font-bold">Reportes</h2>
         </div>
       </div>
-
+    </div>
       <!-- Información del Ciudadano -->
         <div class="flex-shrink-0">
           <svg width="50" height="60" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -150,45 +150,6 @@ const departamentos = ref([
   { name: "Vichada", code: "99" },
 
 ]);
-
-
-const departamentos = ref([
-
-  { name: "Amazonas", code: "91" },
-  { name: "Antioquia", code: "05" },
-  { name: "Arauca", code: "81" },
-  { name: "Atlantico", code: "08" },
-  { name: "Bolivar", code: "13" },
-  { name: "Boyacá", code: "15" },
-  { name: "Caldas", code: "17" },
-  { name: "Caquetá", code: "18" },
-  { name: "Casanare", code: "85" },
-  { name: "Cauca", code: "19" },
-  { name: "Cesar", code: "20" },
-  { name: "Chocó", code: "27" },
-  { name: "Cundinamarca", code: "25" },
-  { name: "Cordoba", code: "23" },
-  { name: "Guainia", code: "94" },
-  { name: "Guaviare", code: "95" },
-  { name: "Huila", code: "41" },
-  { name: "La Guajira", code: "44" },
-  { name: "Magdalena", code: "47" },
-  { name: "Meta", code: "50" },
-  { name: "Nariño", code: "52" },
-  { name: "Norte de Santander", code: "54" },
-  { name: "Putumayo", code: "86" },
-  { name: "Quindio", code: "63" },
-  { name: "Risaralda", code: "66" },
-  { name: "San Andres, Providencia y Santa Catalina", code: "88" },
-  { name: "Santander", code: "68" },
-  { name: "Sucre", code: "70" },
-  { name: "Tolima", code: "73" },
-  { name: "Valle del Cauca", code: "76" },
-  { name: "Vaupés", code: "97" },
-  { name: "Vichada", code: "99" },
-
-]);
-
 </script>
 
 <style scoped>
