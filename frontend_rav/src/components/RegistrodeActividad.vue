bfd57819d117a6e1af2c73853ac72583899e45ad
Alvarezzz14 <andresalvarez14@gmail.com>
2024-12-22 22:18:19 -0500

    flecha categorias morada

--- frontend_rav/src/components/RegistrodeActividad.vue
+++ frontend_rav/src/components/RegistrodeActividad.vue
@@ -156,11 +156,11 @@
 							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
-							stroke="currentColor">
+							stroke="#71277A">
 							<path
 								stroke-linecap="round"
 								stroke-linejoin="round"
-								stroke-width="2"
+								stroke-width="3"
 								d="M19 9l-7 7-7-7" />
 						</svg>
 					</button>
