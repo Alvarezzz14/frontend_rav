95f38085717feec14dacddffce3bc6eff9b36ca9
Matteeoo18 <mateoher93@gmail.com>
2025-11-07 00:33:17 -0500

    Refactor department images and authentication logic
    - Updated department image imports to use shields instead of flags in `departamentos.js`.
    - Commented out authentication checks in the router to disable route protection temporarily in `index.js`.
    - Introduced a default user for development purposes in the authentication store, allowing easier testing in `auth.js`.

--- frontend_rav/src/components/BusquedaDepartamento.vue
+++ frontend_rav/src/components/BusquedaDepartamento.vue
@@ -127,7 +127,7 @@
             :fill="
                 selectedCountry && selectedCountry.code === dep.code
                   ? '#FFD600'
-                  : 'rgba(0, 93, 202, 0.05)'
+                  : '#C3DAFF'
             "
             :stroke="
                 selectedCountry && selectedCountry.code === dep.code
--- frontend_rav/src/data/departamentosMapa/departamentos.js
+++ frontend_rav/src/data/departamentosMapa/departamentos.js
@@ -1,128 +1,128 @@
-import AmazonasBandera from "@/assets/images/webp/amazonas.webp";
+import AmazonasEscudo from "@/assets/images/EscudosDepartamentos/amazonas.svg";
 import AmazonasCiudad from "@/assets/images/imagenDepartamento/Amazonas.webp";
 import AmazonasMapa from "@/assets/images/FragmentoDepartamentos/Amazonas.svg";
 
-import AntioquiaBandera from "@/assets/images/webp/antioquia.webp";
+import AntioquiaEscudo from "@/assets/images/EscudosDepartamentos/antioquia.svg";
 import AntioquiaCiudad from "@/assets/images/imagenDepartamento/antioquia.webp";
 import AntioquiaMapa from "@/assets/images/FragmentoDepartamentos/Antioquia.svg";
 
-import AraucaBandera from "@/assets/images/webp/Arauca.webp";
+import AraucaEscudo from "@/assets/images/EscudosDepartamentos/arauca.svg";
 import AraucaCiudad from "@/assets/images/imagenDepartamento/Arauca.webp";
 import AraucaMapa from "@/assets/images/FragmentoDepartamentos/Arauca.svg";
 
-import AtlanticoBandera from "@/assets/images/webp/atlantico.webp";
+import AtlanticoEscudo from "@/assets/images/EscudosDepartamentos/atlantico.svg";
 import AtlanticoCiudad from "@/assets/images/imagenDepartamento/atlantico.webp";
 import AtlanticoMapa from "@/assets/images/FragmentoDepartamentos/Atlantico.svg";
 
-import BolivarBandera from "@/assets/images/webp/bolivar.webp";
+import BolivarEscudo from "@/assets/images/EscudosDepartamentos/bolivar.svg";
 import BolivarCiudad from "@/assets/images/imagenDepartamento/bolivar.webp";
 import BolivarMapa from "@/assets/images/FragmentoDepartamentos/Bolivar.svg";
 
-import BoyacaBandera from "@/assets/images/webp/boyaca.webp";
+import BoyacaEscudo from "@/assets/images/EscudosDepartamentos/boyaca.svg";
 import BoyacaCiudad from "@/assets/images/imagenDepartamento/boyaca.webp";
 import BoyacaMapa from "@/assets/images/FragmentoDepartamentos/Boyaca.svg";
 
-import CaldasBandera from "@/assets/images/webp/caldas.webp";
+import CaldasEscudo from "@/assets/images/EscudosDepartamentos/caldas.svg";
 import CaldasCiudad from "@/assets/images/imagenDepartamento/caldas.webp";
 import CaldasMapa from "@/assets/images/FragmentoDepartamentos/Caldas.svg";
 
-import CaquetaBandera from "@/assets/images/webp/caqueta.webp";
+import CaquetaEscudo from "@/assets/images/EscudosDepartamentos/caqueta.svg";
 import CaquetaCiudad from "@/assets/images/imagenDepartamento/caqueta.webp";
 import CaquetaMapa from "@/assets/images/FragmentoDepartamentos/Caqueta.svg";
 
-import CasanareBandera from "@/assets/images/webp/casanare.webp";
+import CasanareEscudo from "@/assets/images/EscudosDepartamentos/casanare.svg";
 import CasanareCiudad from "@/assets/images/imagenDepartamento/casanare.webp";
 import CasanareMapa from "@/assets/images/FragmentoDepartamentos/Casanare.svg";
 
-import CaucaBandera from "@/assets/images/webp/cauca.webp";
+import CaucaEscudo from "@/assets/images/EscudosDepartamentos/cauca.svg";
 import CaucaCiudad from "@/assets/images/imagenDepartamento/cauca.webp";
 import CaucaMapa from "@/assets/images/FragmentoDepartamentos/Cauca.svg";
 
-import CesarBandera from "@/assets/images/webp/cesar.webp";
+import cesarEscudo from "@/assets/images/EscudosDepartamentos/cesar.svg";
 import CesarCiudad from "@/assets/images/imagenDepartamento/cesar.webp";
 import CesarMapa from "@/assets/images/FragmentoDepartamentos/Cesar.svg";
 
-import ChocoBandera from "@/assets/images/webp/choco.webp";
+import ChocoEscudo from "@/assets/images/EscudosDepartamentos/choco.svg";
 import ChocoCiudad from "@/assets/images/imagenDepartamento/choco.webp";
 import ChocoMapa from "@/assets/images/FragmentoDepartamentos/Choco.svg";
 
-import CordobaBandera from "@/assets/images/webp/cordoba.webp";
+import CordobaEscudo from "@/assets/images/EscudosDepartamentos/cordoba.svg";
 import CordobaCiudad from "@/assets/images/imagenDepartamento/cordoba.webp";
 import CordobaMapa from "@/assets/images/FragmentoDepartamentos/Cordoba.svg";
 
-import CundinamarcaBandera from "@/assets/images/webp/cundinamarca.webp";
+import CundinamarcaEscudo from "@/assets/images/EscudosDepartamentos/cundinamarca.svg";
 import CundinamarcaCiudad from "@/assets/images/imagenDepartamento/cundinamarca.webp";
 import CundinamarcaMapa from "@/assets/images/FragmentoDepartamentos/Cundinamarca.svg";
 
-import GuainiaBandera from "@/assets/images/webp/guainia.webp";
+import GuainiaEscudo from "@/assets/images/EscudosDepartamentos/guainia.svg";
 import GuainiaCiudad from "@/assets/images/imagenDepartamento/guainia.webp";
 import GuainiaMapa from "@/assets/images/FragmentoDepartamentos/Guainia.svg";
 
-import GuaviareBandera from "@/assets/images/webp/guaviare.webp";
+import GuaviareEscudo from "@/assets/images/EscudosDepartamentos/guaviare.svg";
 import GuaviareCiudad from "@/assets/images/imagenDepartamento/guaviare.webp";
 import GuaviareMapa from "@/assets/images/FragmentoDepartamentos/Guaviare.svg";
 
-import HuilaBandera from "@/assets/images/webp/huila.webp";
+import HuilaEscudo from "@/assets/images/EscudosDepartamentos/huila.svg";
 import HuilaCiudad from "@/assets/images/imagenDepartamento/huila.webp";
 import HuilaMapa from "@/assets/images/FragmentoDepartamentos/Huila.svg";
 
-import GuajiraBandera from "@/assets/images/webp/guajira.webp";
+import GuajiraEscudo from "@/assets/images/EscudosDepartamentos/guajira.svg";
 import GuajiraCiudad from "@/assets/images/imagenDepartamento/guajira.webp";
 import GuajiraMapa from "@/assets/images/FragmentoDepartamentos/Guajira.svg";
 
-import MagdalenaBandera from "@/assets/images/webp/magdalena.webp";
+import MagdalenaEscudo from "@/assets/images/EscudosDepartamentos/magdalena.svg";
 import MagdalenaCiudad from "@/assets/images/imagenDepartamento/magdalena.webp";
 import MagdalenaMapa from "@/assets/images/FragmentoDepartamentos/Magdalena.svg";
 
-import MetaBandera from "@/assets/images/webp/meta.webp";
+import MetaEscudo from "@/assets/images/EscudosDepartamentos/meta.svg";
 import MetaCiudad from "@/assets/images/imagenDepartamento/meta.webp";
 import MetaMapa from "@/assets/images/FragmentoDepartamentos/Meta.svg";
 
-import NariñoBandera from "@/assets/images/webp/nariño.webp";
+import NariñoEscudo from "@/assets/images/EscudosDepartamentos/nariño.svg";
 import NariñoCiudad from "@/assets/images/imagenDepartamento/narino.webp";
 import NariñoMapa from "@/assets/images/FragmentoDepartamentos/Nariño.svg";
 
-import NorteSantanderBandera from "@/assets/images/webp/norte-de-santander.webp";
+import NorteSantanderEscudo from "@/assets/images/EscudosDepartamentos/norte-santander.svg";
 import NorteSantanderCiudad from "@/assets/images/imagenDepartamento/norteSantander.webp";
 import NorteSantanderMapa from "@/assets/images/FragmentoDepartamentos/Norte-de-Santander.svg";
 
-import PutumayoBandera from "@/assets/images/webp/putumayo.webp";
+import PutumayoEscudo from "@/assets/images/EscudosDepartamentos/putumayo.svg";
 import PutumayoCiudad from "@/assets/images/imagenDepartamento/putumayo.webp";
 import PutumayoMapa from "@/assets/images/FragmentoDepartamentos/Putumayo.svg";
 
-import QuindioBandera from "@/assets/images/webp/quindio.webp";
+import QuindioEscudo from "@/assets/images/EscudosDepartamentos/quindio.svg";
 import QuindioCiudad from "@/assets/images/imagenDepartamento/quindio.webp";
 import QuindioMapa from "@/assets/images/FragmentoDepartamentos/Quindio.svg";
 
-import RisaraldaBandera from "@/assets/images/webp/risaralda.webp";
+import RisaraldaEscudo from "@/assets/images/EscudosDepartamentos/risaralda.svg";
 import RisaraldaCiudad from "@/assets/images/imagenDepartamento/risaralda.webp";
 import RisaraldaMapa from "@/assets/images/FragmentoDepartamentos/Risaralda.svg";
 
-import SanAndresBandera from "@/assets/images/webp/san-andres-y-providencia.webp";
+import SanAndresEscudo from "@/assets/images/EscudosDepartamentos/san-andres.svg";
 import SanAndresCiudad from "@/assets/images/imagenDepartamento/san-andres.webp";
 import SanAndresMapa from "@/assets/images/FragmentoDepartamentos/San-andres.svg";
 
-import SantanderBandera from "@/assets/images/webp/santander.webp";
+import SantanderEscudo from "@/assets/images/EscudosDepartamentos/santander.svg";
 import SantanderCiudad from "@/assets/images/imagenDepartamento/santander.webp";
 import SantanderMapa from "@/assets/images/FragmentoDepartamentos/Santander.svg";
 
-import SucreBandera from "@/assets/images/webp/sucre.webp";
+import SucreEscudo from "@/assets/images/EscudosDepartamentos/sucre.svg";
 import SucreCiudad from "@/assets/images/imagenDepartamento/sucre.webp";
 import SucreMapa from "@/assets/images/FragmentoDepartamentos/Sucre.svg";
 
-import TolimaBandera from "@/assets/images/webp/tolima.webp";
+import TolimaEscudo from "@/assets/images/EscudosDepartamentos/tolima.svg";
 import TolimaCiudad from "@/assets/images/imagenDepartamento/Tolima.webp";
 import TolimaMapa from "@/assets/images/FragmentoDepartamentos/Tolima.svg";
 
-import ValleCaucaBandera from "@/assets/images/webp/valle-del-cauca.webp";
+import ValleCaucaEscudo from "@/assets/images/EscudosDepartamentos/valle-cauca.svg";
 import ValleCaucaCiudad from "@/assets/images/imagenDepartamento/valleCauca.webp";
 import ValleCaucaMapa from "@/assets/images/FragmentoDepartamentos/Valle-del-cauca.svg";
 
-import VaupesBandera from "@/assets/images/webp/vaupes.webp";
+import VaupesEscudo from "@/assets/images/EscudosDepartamentos/vaupes.svg";
 import VaupesCiudad from "@/assets/images/imagenDepartamento/vaupes.webp";
 import VaupesMapa from "@/assets/images/FragmentoDepartamentos/Vaupes.svg";
 
-import VichadaBandera from "@/assets/images/webp/vichada.webp";
+import VichadaEscudo from "@/assets/images/EscudosDepartamentos/vichada.svg";
 import VichadaCiudad from "@/assets/images/imagenDepartamento/vichada.webp";
 import VichadaMapa from "@/assets/images/FragmentoDepartamentos/Vichada.svg";
 
@@ -130,224 +130,224 @@ export const departamentos = [
     {
         name: "Amazonas",
         code: "91",
-        flagUrl: AmazonasBandera,
+        flagUrl: AmazonasEscudo,
         imageUrl: AmazonasCiudad,
         imageMap: AmazonasMapa,
     },
     {
         name: "Antioquia",
         code: "05",
-        flagUrl: AntioquiaBandera,
+        flagUrl: AntioquiaEscudo,
         imageUrl: AntioquiaCiudad,
         imageMap: AntioquiaMapa,
     },
     {
         name: "Arauca",
         code: "81",
-        flagUrl: AraucaBandera,
+        flagUrl: AraucaEscudo,
         imageUrl: AraucaCiudad,
         imageMap: AraucaMapa,
     },
     {
         name: "Atlantico",
         code: "08",
-        flagUrl: AtlanticoBandera,
+        flagUrl: AtlanticoEscudo,
         imageUrl: AtlanticoCiudad,
         imageMap: AtlanticoMapa,
     },
     {
         name: "Bolivar",
         code: "13",
-        flagUrl: BolivarBandera,
+        flagUrl: BolivarEscudo,
         imageUrl: BolivarCiudad,
         imageMap: BolivarMapa,
     },
     {
         name: "Boyacá",
         code: "15",
-        flagUrl: BoyacaBandera,
+        flagUrl: BoyacaEscudo,
         imageUrl: BoyacaCiudad,
         imageMap: BoyacaMapa,
     },
     {
         name: "Caldas",
         code: "17",
-        flagUrl: CaldasBandera,
+        flagUrl: CaldasEscudo,
         imageUrl: CaldasCiudad,
         imageMap: CaldasMapa,
     },
     {
         name: "Caquetá",
         code: "18",
-        flagUrl: CaquetaBandera,
+        flagUrl: CaquetaEscudo,
         imageUrl: CaquetaCiudad,
         imageMap: CaquetaMapa,
     },
     {
         name: "Casanare",
         code: "85",
-        flagUrl: CasanareBandera,
+        flagUrl: CasanareEscudo,
         imageUrl: CasanareCiudad,
         imageMap: CasanareMapa,
     },
     {
         name: "Cauca",
         code: "19",
-        flagUrl: CaucaBandera,
+        flagUrl: CaucaEscudo,
         imageUrl: CaucaCiudad,
         imageMap: CaucaMapa,
     },
     {
         name: "Cesar",
         code: "20",
-        flagUrl: CesarBandera,
+        flagUrl: cesarEscudo,
         imageUrl: CesarCiudad,
         imageMap: CesarMapa,
     },
     {
         name: "Chocó",
         code: "27",
-        flagUrl: ChocoBandera,
+        flagUrl: ChocoEscudo,
         imageUrl: ChocoCiudad,
         imageMap: ChocoMapa,
     },
     {
         name: "Cundinamarca",
         code: "25",
-        flagUrl: CundinamarcaBandera,
+        flagUrl: CundinamarcaEscudo,
         imageUrl: CundinamarcaCiudad,
         imageMap: CundinamarcaMapa,
     },
     {
         name: "Cordoba",
         code: "23",
-        flagUrl: CordobaBandera,
+        flagUrl: CordobaEscudo,
         imageUrl: CordobaCiudad,
         imageMap: CordobaMapa,
     },
     {
         name: "Guainia",
         code: "94",
-        flagUrl: GuainiaBandera,
+        flagUrl: GuainiaEscudo,
         imageUrl: GuainiaCiudad,
         imageMap: GuainiaMapa,
     },
     {
         name: "Guaviare",
         code: "95",
-        flagUrl: GuaviareBandera,
-        imageUrl: GuaviareCiudad,
-        imageMap: GuaviareMapa,
+        flagUrl: GuaviareEscudo,
+        imageUrl: GuaviareCiudad, 
+        imageMap:GuaviareMapa,
     },
     {
         name: "Huila",
         code: "41",
-        flagUrl: HuilaBandera,
+        flagUrl: HuilaEscudo,
         imageUrl: HuilaCiudad,
         imageMap: HuilaMapa,
     },
     {
         name: "La Guajira",
         code: "44",
-        flagUrl: GuajiraBandera,
+        flagUrl: GuajiraEscudo,
         imageUrl: GuajiraCiudad,
         imageMap: GuajiraMapa,
     },
     {
         name: "Magdalena",
         code: "47",
-        flagUrl: MagdalenaBandera,
+        flagUrl: MagdalenaEscudo,
         imageUrl: MagdalenaCiudad,
         imageMap: MagdalenaMapa,
     },
     {
         name: "Meta",
         code: "50",
-        flagUrl: MetaBandera,
+        flagUrl: MetaEscudo,
         imageUrl: MetaCiudad,
         imageMap: MetaMapa,
     },
     {
         name: "Nariño",
         code: "52",
-        flagUrl: NariñoBandera,
+        flagUrl: NariñoEscudo,
         imageUrl: NariñoCiudad,
         imageMap: NariñoMapa,
     },
     {
         name: "Norte de Santander",
         code: "54",
-        flagUrl: NorteSantanderBandera,
+        flagUrl: NorteSantanderEscudo,
         imageUrl: NorteSantanderCiudad,
         imageMap: NorteSantanderMapa,
     },
     {
         name: "Putumayo",
         code: "86",
-        flagUrl: PutumayoBandera,
+        flagUrl: PutumayoEscudo,
         imageUrl: PutumayoCiudad,
         imageMap: PutumayoMapa,
     },
     {
         name: "Quindio",
         code: "63",
-        flagUrl: QuindioBandera,
+        flagUrl: QuindioEscudo,
         imageUrl: QuindioCiudad,
         imageMap: QuindioMapa,
     },
     {
         name: "Risaralda",
         code: "66",
-        flagUrl: RisaraldaBandera,
+        flagUrl: RisaraldaEscudo,
         imageUrl: RisaraldaCiudad,
         imageMap: RisaraldaMapa,
     },
     {
         name: "San Andres, Providencia y Santa Catalina",
         code: "88",
-        flagUrl: SanAndresBandera,
+        flagUrl: SanAndresEscudo,
         imageUrl: SanAndresCiudad,
         imageMap: SanAndresMapa,
     },
     {
         name: "Santander",
         code: "68",
-        flagUrl: SantanderBandera,
+        flagUrl: SantanderEscudo,
         imageUrl: SantanderCiudad,
         imageMap: SantanderMapa,
     },
     {
         name: "Sucre",
         code: "70",
-        flagUrl: SucreBandera,
+        flagUrl: SucreEscudo,
         imageUrl: SucreCiudad,
         imageMap: SucreMapa,
     },
     {
         name: "Tolima",
         code: "73",
-        flagUrl: TolimaBandera,
+        flagUrl: TolimaEscudo,
         imageUrl: TolimaCiudad,
         imageMap: TolimaMapa,
     },
     {
         name: "Valle del Cauca",
         code: "76",
-        flagUrl: ValleCaucaBandera,
+        flagUrl: ValleCaucaEscudo,
         imageUrl: ValleCaucaCiudad,
         imageMap: ValleCaucaMapa,
     },
     {
         name: "Vaupés",
         code: "97",
-        flagUrl: VaupesBandera,
+        flagUrl: VaupesEscudo,
         imageUrl: VaupesCiudad,
         imageMap: VaupesMapa,
     },
     {
         name: "Vichada",
         code: "99",
-        flagUrl: VichadaBandera,
+        flagUrl: VichadaEscudo,
         imageUrl: VichadaCiudad,
         imageMap: VichadaMapa,
     },
